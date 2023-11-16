8cb67ff3d79327b42ea488853382c81f51e467d6
luis llamas <luis.llamas@maslla.com>
2023-11-16 15:17:34 -0600

    added new css

--- README.md
+++ README.md
@@ -1,44 +1,31 @@
-# 03 JavaScript: Password Generator
+## Project Name
+JavaScript Password Generator 
 
-## Your Task
+## Version
+1.0.0 Deployment Date: 2023-11-16 Contributors: Luis Llamas
 
-Create an application that enables employees to generate random passwords based on criteria that they’ve selected. This app will run in the browser and will feature dynamically updated HTML and CSS powered by JavaScript code that you write. It will have a clean and polished, responsive user interface that adapts to multiple screen sizes.
+## Description
+
+Create an application that enables users to generate random passwords based on criteria that they’ve selected. This app will run in the browser and will feature dynamically updated HTML and CSS powered by JavaScript. It will have a clean and polished, responsive user interface that adapts to multiple screen sizes.
 
 The password can include special characters. If you’re unfamiliar with these, see this [list of password special characters](https://www.owasp.org/index.php/Password_special_characters) from the OWASP Foundation.
 
-## User Story
+## Technologies
 
-```
-AS AN employee with access to sensitive data
-I WANT to randomly generate a password that meets certain criteria
-SO THAT I can create a strong password that provides greater security
-```
+This project was built using HTML, CSS and JavaScript.
 
-## Acceptance Criteria
+## Installation
+To install this project, download the repository files to your local machine. Open the index.html file in your browser to view the page. To view the code, open the repository folder in your preferred code editor.
 
-```
-GIVEN I need a new, secure password
-WHEN I click the button to generate a password
-THEN I am presented with a series of prompts for password criteria
-WHEN prompted for password criteria
-THEN I select which criteria to include in the password
-WHEN prompted for the length of the password
-THEN I choose a length of at least 8 characters and no more than 128 characters
-WHEN asked for character types to include in the password
-THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
-WHEN I answer each prompt
-THEN my input should be validated and at least one character type should be selected
-WHEN all prompts are answered
-THEN a password is generated that matches the selected criteria
-WHEN the password is generated
-THEN the password is either displayed in an alert or written to the page
-```
+## Usage
+To use this project, open the index.html file in your browser. The page will display the password generator page. Click on the links generate a random password.
 
-## Mock-Up
 
 The following image shows the web application's appearance and functionality:
 
 ![The Password Generator application displays a red button to "Generate Password".](./Assets/03-javascript-homework-demo.png)
 
+## Code
 URL to code: https://github.com/maslla100/password-generator
+## Password Generator Site
 URL to password generator app: https://maslla100.github.io/password-generator/
--- script.js
+++ script.js
@@ -2,15 +2,14 @@
 
 // Function to prompt user for password criteria and generate password
 function generatePassword() {
-    // Initialize password variables
     var password = "";
     var passwordArray = [];
 
-    // Prompt user for password length and validate input
-    var passwordLength = parseInt(prompt("How long would you like your password to be? (8-128 characters)"));
-    while (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
-        alert("Password length must be between 8 and 128 characters.");
-        passwordLength = parseInt(prompt("How long would you like your password to be? (8-128 characters)"));
+    // Get password length from promptInput and validate input
+    var passwordLength = parseInt(document.querySelector("#promptInput").value);
+    if (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
+        displayErrorMessage("Password length must be between 8 and 128 characters.");
+        return "";
     }
 
     // Confirm password criteria with the user
@@ -21,7 +20,7 @@ function generatePassword() {
 
     // Validate that at least one character type has been selected
     if (!includeLowercase && !includeUppercase && !includeNumbers && !includeSymbols) {
-        alert("You must select at least one character type.");
+        displayErrorMessage("You must select at least one character type.");
         return "";
     }
 
@@ -31,16 +30,47 @@ function generatePassword() {
     if (includeNumbers) passwordArray.push("0123456789");
     if (includeSymbols) passwordArray.push("!@#$%^&*()_+-=[]{}");
 
-    // Flatten the passwordArray and remove commas
     var possibleCharacters = passwordArray.join("");
 
-    // Generate password with the specified length and criteria
+    // Generate password
     for (var i = 0; i < passwordLength; i++) {
         password += possibleCharacters.charAt(Math.floor(Math.random() * possibleCharacters.length));
     }
 
-    // Return the generated password
+    // Display the password length message
+    displayPasswordLengthMsg(passwordLength);
+
     return password;
+}
+
+// Function to display error messages on the webpage
+function displayErrorMessage(message) {
+    alert(message); // Placeholder for your implementation
+}
+
+// Function to display password length message
+function displayPasswordLengthMsg(length) {
+    var message = length > 0 ? "A " + length + "-character password has been generated." : "";
+    document.querySelector("#passwordLengthMsg").textContent = message;
+}
+
+// Function to copy password to clipboard
+function copyToClipboard() {
+    var password = document.querySelector("#password").value;
+    if (password) {
+        var passwordField = document.querySelector("#password");
+        passwordField.select();
+        document.execCommand("copy");
+        alert("Password copied to clipboard!");
+    } else {
+        alert("No password to copy."); // Message when there is no password
+    }
+}
+
+
+// Function to toggle dark mode
+function toggleDarkMode() {
+    document.body.classList.toggle('dark-mode');
 }
 
 // Event listener for the Generate Password button
@@ -50,3 +80,56 @@ generateBtn.addEventListener("click", function () {
     var passwordText = document.querySelector("#password");
     passwordText.value = password;
 });
+
+// Event listener for the Copy Password button
+var copyBtn = document.querySelector("#copy");
+copyBtn.addEventListener("click", copyToClipboard);
+
+// Event listener for the Dark Mode Toggle button
+var darkModeBtn = document.querySelector("#darkModeToggle");
+darkModeBtn.addEventListener("click", toggleDarkMode);
+
+// Function to reset the application
+function resetApplication() {
+    // Clear the password input field
+    document.querySelector("#promptInput").value = '';
+
+    // Clear the generated password field
+    var passwordText = document.querySelector("#password");
+    passwordText.value = '';
+
+    // Clear any displayed messages
+    document.querySelector("#passwordLengthMsg").textContent = '';
+}
+
+// Event listener for the Reset button
+var resetBtn = document.querySelector("#reset");
+resetBtn.addEventListener("click", resetApplication);
+
+// Function to reset the application and provide feedback
+function resetApplication() {
+    var passwordInput = document.querySelector("#promptInput");
+    var passwordText = document.querySelector("#password");
+    var messageBox = document.querySelector("#passwordLengthMsg");
+
+    // Check if there's anything to reset
+    if (passwordInput.value === '' && passwordText.value === '') {
+        alert("Nothing to reset. Please input your password length and generate a password first.");
+    } else {
+        // Clear the password input field
+        passwordInput.value = '';
+
+        // Clear the generated password field
+        passwordText.value = '';
+
+        // Clear any displayed messages and show reset confirmation
+        messageBox.textContent = 'The application has been reset.';
+
+        // Optional: Clear the reset confirmation message after a delay
+        setTimeout(function () {
+            messageBox.textContent = '';
+        }, 3000); // Clears the message after 3 seconds
+    }
+}
+
+
